f831bd6d439db6414238105cf37b5590f8eb08f3
unknown <leon.pontes@laceti.cin.ufpe.br>
2021-10-16 11:34:52 -0300

    challenge complete

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,16 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (user.admin === false) {
+      throw new Error("User is not admin");
+    }
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
